7c95123c83ee4353eead0e2f8fc3bb6c5cf02ee4
Bo Peng <xbopeng@outlook.com>
2018-02-18 20:21:22 -0600

    data processing file changed

--- dataProc.py
+++ dataProc.py
@@ -87,7 +87,7 @@ def image_normaliztion(im):
     
     return im_norm
 
-def patch_ceter_point(im_width, im_height, im_patch_width, im_patch_height, lb_patch_width, lb_patch_height, nIm):
+def patch_center_point(im_width, im_height, im_patch_width, im_patch_height, lb_patch_width, lb_patch_height, nIm):
     """
     Compute the center point coordinates of the patch in raw images
     INPUT:
@@ -99,18 +99,29 @@ def patch_ceter_point(im_width, im_height, im_patch_width, im_patch_height, lb_p
     """
     # for each image, the pathc center coordinates are the same
     patch_cpt = []
-    num_patch_width = (im_width - im_patch_width) / lb_patch_width + 1
-    num_patch_height = (im_height - im_patch_height) / lb_patch_height + 1
+    num_patch_width = int((im_width - im_patch_width) / lb_patch_width + 1)
+    num_patch_height = int((im_height - im_patch_height) / lb_patch_height + 1)
     for k in range(nIm):
         for i in range(num_patch_height):
-            cpt_i = im_patch_height / 2 + i * lb_patch_height # coordinate: y from up to bottom
+            cpt_i = int(im_patch_height / 2) + i * lb_patch_height # coordinate: y from up to bottom
             for j in range(num_patch_width):                
-                cpt_j = im_patch_width / 2 + j * lb_patch_width # coordinate: x from left to right
+                cpt_j = int(im_patch_width / 2) + j * lb_patch_width # coordinate: x from left to right
                 patch_cpt.append([k, cpt_j, cpt_i])
                 
     return patch_cpt
 
-def data_batch(image, label, patch_cpt, im_patch_width, im_patch_height, lb_patch_width, lb_patch_height, batch_size, batch_idx):
+#patch_cpt = patch_center_point(1500, 1500, 64, 64, 16, 16, 100)
+#print(patch_cpt)
+
+def data_batch(image, 
+            label, 
+            patch_cpt, 
+            im_patch_width, 
+            im_patch_height, 
+            lb_patch_width, 
+            lb_patch_height, 
+            batch_size, 
+            batch_idx):
     """
     Extract image patches for training according to patch_cpt
     Each images patches are overlapped to make label patches exactly join together without overlap or gap
@@ -125,10 +136,15 @@ def data_batch(image, label, patch_cpt, im_patch_width, im_patch_height, lb_patc
         batch images with corresponding labels of size batch_size
     """
     idx_patch_start = batch_size * batch_idx # starting index of the first image patch in current patch
+    idx_patch_end = idx_patch_start+batch_size
+    # the last batch may have size greater than the batch_size
+    if idx_patch_end > len(patch_cpt):
+        idx_patch_end = len(patch_cpt)
     im_patch_batch = [] # array to store images patches in current batch
     lb_patch_batch = [] # array to store label patches in current batch
     
-    for i in range(idx_patch_start, idx_patch_start+batch_size):
+
+    for i in range(idx_patch_start, idx_patch_end):
         # add each image patch to [im_patch_batch]
         idx_im = patch_cpt[i][0] # index of raw images
         # coordinate system: x means pixel moving from left to right, y means pixel moving from up to bottom
@@ -136,10 +152,10 @@ def data_batch(image, label, patch_cpt, im_patch_width, im_patch_height, lb_patc
         y_patch_cpt = patch_cpt[i][2] # y ...
         
         # note that image patch and label patch have different sizes
-        x_left_im = x_patch_cpt - im_patch_width / 2 # left most coordinate of the patch
-        y_up_im   = y_patch_cpt - im_patch_height / 2 # upper most coordinate of the patch
-        x_left_lb = x_patch_cpt - lb_patch_width / 2
-        y_up_lb   = y_patch_cpt - lb_patch_height / 2
+        x_left_im = x_patch_cpt - int(im_patch_width / 2) # left most coordinate of the patch
+        y_up_im   = y_patch_cpt - int(im_patch_height / 2) # upper most coordinate of the patch
+        x_left_lb = x_patch_cpt - int(lb_patch_width / 2)
+        y_up_lb   = y_patch_cpt - int(lb_patch_height / 2)
         
         # extract the patch
         im_patch = image[idx_im][x_left_im:x_left_im+im_patch_width, y_up_im:y_up_im+im_patch_height] # in python, [a:b] does not include b but less than b
@@ -150,3 +166,101 @@ def data_batch(image, label, patch_cpt, im_patch_width, im_patch_height, lb_patc
         lb_patch_batch.append(lb_patch)
     
     return im_patch_batch, lb_patch_batch
+
+def data_patch(image,
+            label, 
+            patch_cpt, 
+            im_patch_width, 
+            im_patch_height, 
+            lb_patch_width, 
+            lb_patch_height):
+    """
+    transform raw images into small patches
+    """
+    im_patch_all = []
+    lb_patch_all = []
+
+    for i in range(len(patch_cpt)):
+        idx_im = patch_cpt[i][0] # index of raw images
+        # coordinate system: x means pixel moving from left to right, y means pixel moving from up to bottom
+        x_patch_cpt = patch_cpt[i][1] # x coordinate of center point
+        y_patch_cpt = patch_cpt[i][2] # y ...
+
+        # note that image patch and label patch have different sizes
+        x_left_im = x_patch_cpt - int(im_patch_width / 2) # left most coordinate of the patch
+        y_up_im   = y_patch_cpt - int(im_patch_height / 2) # upper most coordinate of the patch
+        x_left_lb = x_patch_cpt - int(lb_patch_width / 2)
+        y_up_lb   = y_patch_cpt - int(lb_patch_height / 2)
+
+        # extract the patch
+        im_patch = image[idx_im][x_left_im:x_left_im+im_patch_width, y_up_im:y_up_im+im_patch_height] # in python, [a:b] does not include b but less than b
+        lb_patch = label[idx_im][x_left_lb:x_left_lb+lb_patch_width, y_up_lb:y_up_lb+lb_patch_height]
+        
+        # add this patch to the array
+        im_patch_all.append(im_patch)
+        lb_patch_all.append(lb_patch)
+    
+    return im_patch_all, lb_patch_all
+
+def data_patch_batch_random(image,
+                            label, 
+                            patch_cpt, 
+                            im_patch_width, 
+                            im_patch_height, 
+                            lb_patch_width, 
+                            lb_patch_height,
+                            batch_size):
+    """
+    transform raw images into small patches
+    """
+    im_patch_batch = []
+    lb_patch_batch = []
+
+    patch_idx = random.sample(range(len(patch_cpt)), batch_size)
+
+    for i in patch_idx:
+        idx_im = patch_cpt[i][0] # index of raw images
+        # coordinate system: x means pixel moving from left to right, y means pixel moving from up to bottom
+        x_patch_cpt = patch_cpt[i][1] # x coordinate of center point
+        y_patch_cpt = patch_cpt[i][2] # y ...
+
+        # note that image patch and label patch have different sizes
+        x_left_im = x_patch_cpt - int(im_patch_width / 2) # left most coordinate of the patch
+        y_up_im   = y_patch_cpt - int(im_patch_height / 2) # upper most coordinate of the patch
+        x_left_lb = x_patch_cpt - int(lb_patch_width / 2)
+        y_up_lb   = y_patch_cpt - int(lb_patch_height / 2)
+
+        # extract the patch
+        im_patch = image[idx_im][x_left_im:x_left_im+im_patch_width, y_up_im:y_up_im+im_patch_height] # in python, [a:b] does not include b but less than b
+        lb_patch = label[idx_im][x_left_lb:x_left_lb+lb_patch_width, y_up_lb:y_up_lb+lb_patch_height]
+        
+        # add this patch to the array
+        im_patch_batch.append(im_patch)
+        lb_patch_batch.append(lb_patch)
+    
+    return im_patch_batch, lb_patch_batch
+
+def image_mosaic(output_patch, patch_cpt):
+    """
+    mosaic image patches into one image
+    """
+
+    output_patch = np.array(output_patch)
+    output_patch = output_patch.astype(np.int32)
+
+    num_batch, batch_size, patch_width, patch_height = np.shape(output_patch)
+    output_label = np.zeros((1500, 1500), dtype=np.int32)
+
+    output_patch = np.reshape(output_patch, (num_batch*batch_size, patch_width, patch_height))
+
+    # number of patches
+    num_patch = num_batch*batch_size
+    
+    # assign patches to the big output label image
+    for k in range(num_patch):
+        cpt_x = patch_cpt[k][1]
+        cpt_y = patch_cpt[k][2]
+        output_label[cpt_y-8:cpt_y+8, cpt_x-8:cpt_x+8] = output_patch[k]
+
+    return output_label
+    
